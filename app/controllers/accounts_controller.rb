5e2dbea80db576c679cae537cefb94d8ed4243ba
aprwebmaster <cbradford@apr.com>
2018-01-02 15:11:34 -0800

    added search for accounts

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -3,6 +3,12 @@ class AccountsController < ApplicationController
     @accounts = Account.order(:name).paginate(page: params[:page], per_page: 10)
     authorize @accounts 
 
+  if params[:search]
+    @accounts = Account.search(params[:search]).order("created_at DESC").paginate(page: params[:page], per_page: 10)
+  else
+    @accounts = Account.order(:name).paginate(page: params[:page], per_page: 10)
+  end
+
   end
 
   def show
--- app/models/account.rb
+++ app/models/account.rb
@@ -3,6 +3,9 @@ class Account < ApplicationRecord
 	has_many :service_comments, dependent: :destroy
 	has_many :stores, dependent: :destroy
 
+	def self.search(search)
+    where("name ILIKE ? OR account_number ILIKE ? OR code ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
+    end
 
 
 end
